e98cac0f129e72b63426d241b88294a2727bc647
mm8564a <max.mcdermott05@icloud.com>
2022-10-03 14:10:15 -0400

    End Day 3

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+const fs = require('node:fs');
+const path = require('node:path');
 // Require the necessary discord.js classes
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
     console.log('Ready!');
@@ -12,21 +36,17 @@ client.once('ready', () => {
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
- 
-    const { commandName } = interaction;
-
-    if (commandName === 'robloxPro?') {
-        await interaction.reply('MoreLikeRobloxNoob!');
-    } else if (commandName === 'server') {
-        await interaction.reply('Server info.');
-    } else if (commandName === 'user') {
-        await interaction.reply('User info.');
-    } else if (commandName === 'hello'){
-        await interaction.reply('howdy!')
-    }
-
-});
-// Login to Discord with your client's token
+        const command = interaction.client.commands.get(interaction.commandName);
+    
+        if (!command) return;
+    
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    });
 
 
 client.login(token);
